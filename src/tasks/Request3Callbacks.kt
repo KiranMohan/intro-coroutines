0ac6394f0152f6986b5bd3a951279cc5de2551c9
Kiran Mohan <kiran.mohan@123.com>
2021-06-06 23:21:35 +0530

    fix loadContributorsCallbacks 2

--- src/tasks/Request3Callbacks.kt
+++ src/tasks/Request3Callbacks.kt
@@ -1,26 +1,35 @@
 package tasks
 
 import contributors.*
+import mu.KotlinLogging
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import java.util.*
 import java.util.concurrent.atomic.AtomicInteger
 
+private val logger = KotlinLogging.logger {}
+
 fun loadContributorsCallbacks(service: GitHubService, req: RequestData, updateResults: (List<User>) -> Unit) {
     service.getOrgReposCall(req.org).onResponse { responseRepos ->
+        log.debug("In getOrgReposCall().onResponse()")
         logRepos(req, responseRepos)
         val repos = responseRepos.bodyList()
-        val allUsers = mutableListOf<User>()
+        val allUsers = Collections.synchronizedList(mutableListOf<User>())
+        val indexCount = AtomicInteger(0)
         for (repo in repos) {
             service.getRepoContributorsCall(req.org, repo.name).onResponse { responseUsers ->
+                log.debug("In getRepoContributorsCall().onResponse()")
                 logUsers(repo, responseUsers)
                 val users = responseUsers.bodyList()
                 allUsers += users
+                if(indexCount.incrementAndGet() == repos.lastIndex) {
+                    updateResults(allUsers.aggregate())
+                }
             }
         }
+
         // TODO: Why this code doesn't work? How to fix that?
-        updateResults(allUsers.aggregate())
     }
 }
 
