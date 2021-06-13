3d40432e04d1b1ebc04f61a7031c6b48b5ffcd51
Kiran Mohan <kiran.mohan@123.com>
2021-06-13 23:20:40 +0530

    channels

--- src/tasks/Request7Channels.kt
+++ src/tasks/Request7Channels.kt
@@ -1,16 +1,40 @@
 package tasks
 
 import contributors.*
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalCoroutinesApi::class)
 suspend fun loadContributorsChannels(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
     coroutineScope {
-        TODO()
+        val repos = service
+            .getOrgRepos(req.org)
+            .also { logRepos(req, it) }
+            .bodyList()
+
+        val channel = Channel<List<User>>()
+        for ((index, repo) in repos.withIndex()) {
+            launch {
+                val users = service.getRepoContributors(req.org, repo.name)
+                    .also { logUsers(repo, it) }
+                    .bodyList()
+
+                channel.send(users)
+            }
+
+        }
+
+        var allUsers = emptyList<User>()
+        repeat(repos.size) {
+            val users = channel.receive()
+            allUsers = (allUsers + users).aggregate()
+            updateResults(allUsers, it == repos.size)
+        }
     }
 }
